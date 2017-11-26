eff1aedd3b68786c4dec9c768f61d07a0bf48394
magnus-untal <33887026+magnus-untal@users.noreply.github.com>
2017-11-26 18:47:04 +0800

    Add files via upload

--- Values.java
+++ Values.java
@@ -6,18 +6,23 @@ public class Values{
 	private final long RECORD_COUNT_OFFSET = 0;
 	private RandomAccessFile valuesFile;
 	
-	public Values(String name) throws IOException{
-		File file = new File(name);
-		if(file.exists()){
-			valuesFile = new RandomAccessFile(name, "rwd");
-			valuesFile.seek(RECORD_COUNT_OFFSET);
-			recordCount = valuesFile.readLong() + 1;
+	public Values(String name){
+		try{
+			File file = new File(name);
+			if(file.exists()){
+				valuesFile = new RandomAccessFile(name, "rwd");
+				valuesFile.seek(RECORD_COUNT_OFFSET);
+				recordCount = valuesFile.readLong() + 1;
+			}
+			else{
+				recordCount = 0;
+				valuesFile = new RandomAccessFile(name, "rwd");
+				valuesFile.seek(RECORD_COUNT_OFFSET);
+				valuesFile.writeLong(recordCount);
+			}
 		}
-		else{
-			recordCount = 0;
-			valuesFile = new RandomAccessFile(name, "rwd");
-			valuesFile.seek(RECORD_COUNT_OFFSET);
-			valuesFile.writeLong(recordCount);
+		catch(IOException ex){
+			ex.printStackTrace(System.out);
 		}
 	}
 	
@@ -25,11 +30,18 @@ public class Values{
 		valuesFile.seek(0);
 		valuesFile.writeLong(recordCount);
 		valuesFile.seek(8+recordCount*BYTE_LENGTH);
-		valuesFile.writeLong(recordCount);
+		register(recordCount,b);
+		recordCount++;
+	}
+
+	public void updateFile(byte[] b, long recordNum) throws IOException{
+		valuesFile.seek(8+recordNum*BYTE_LENGTH);
+		register(recordNum, b);
+	}
+
+	private void register(long record, byte[] b) throws IOException{
+		valuesFile.writeLong(record);
 		valuesFile.writeShort(b.length);
 		valuesFile.write(b);
-		recordCount++;
-		//System.out.println(recordCount);
-		//System.out.println(RECORD_COUNT_OFFSET);
 	}
 }
