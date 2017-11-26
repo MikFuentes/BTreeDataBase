dcd752df98e2fccacad6b5ba1504733f9241019d
magnus-untal <33887026+magnus-untal@users.noreply.github.com>
2017-11-26 16:34:35 +0800

    Add files via upload

--- Values.java
+++ Values.java
@@ -1,17 +1,17 @@
 import java.io.*;
 
 public class Values{
-	private final int BYTE_LENGTH = 256;
-	private final short RECORD_COUNT_OFFSET = 2;
-	private RandomAccessFile valuesFile;
 	private long recordCount;
+	private final int BYTE_LENGTH = 256;
+	private final long RECORD_COUNT_OFFSET = 0;
+	private RandomAccessFile valuesFile;
 	
 	public Values(String name) throws IOException{
 		File file = new File(name);
 		if(file.exists()){
 			valuesFile = new RandomAccessFile(name, "rwd");
 			valuesFile.seek(RECORD_COUNT_OFFSET);
-			recordCount = valuesFile.readLong();
+			recordCount = valuesFile.readLong() + 1;
 		}
 		else{
 			recordCount = 0;
@@ -22,9 +22,14 @@ public class Values{
 	}
 	
 	public void writeToFile(byte[] b) throws IOException{
+		valuesFile.seek(0);
 		valuesFile.writeLong(recordCount);
-		valuesFile.write(b.length);
+		valuesFile.seek(8+recordCount*BYTE_LENGTH);
+		valuesFile.writeLong(recordCount);
+		valuesFile.writeShort(b.length);
 		valuesFile.write(b);
 		recordCount++;
+		//System.out.println(recordCount);
+		//System.out.println(RECORD_COUNT_OFFSET);
 	}
 }
