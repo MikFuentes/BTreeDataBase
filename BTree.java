b5e9cae203a09257013553403273ed977709502a
MikFuentes <33886316+MikFuentes@users.noreply.github.com>
2017-11-28 11:42:22 +0800

    Update BTree.java

--- BTree.java
+++ BTree.java
@@ -1,10 +1,13 @@
 import java.io.*;
+import java.util.*;
 
 public class BTree{
 	private final long RECORD_COUNT_OFFSET = 0;
 	private final int ORDER = 7;
 	private long nodeCount;
 	private RandomAccessFile bTreeFile;
+	private ArrayList<Node> nodeList;
+
 	public BTree(String name){
 		try{
 			File file = new File(name);
@@ -22,20 +25,30 @@ public class BTree{
 			ex.printStackTrace(System.out);
 		}
 	}
-	public void writeToFile(byte[] b){
+	public void split(){ //method for spliiting B-Tree **to be continued
+
+	}
+	public void writeToFile(byte[] b){ //**incomplete???
 		
 	}
 
-	class Node{
+
+	class Node{ //node class
 		private long[] pointer;
-		public Node(){
-			pointer = new long[3*ORDER-1];
+		public Node(){ //each node has a long[]
+			pointer = new long[3*ORDER-1]; //long[] of size 20
 			for(int i = 0; i<pointer.length; i++){
-				pointer[i] = -1;
+				pointer[i] = -1; //each index gains a value of -1
 			}
 		}
-		public boolean isRoot(Node node){
-			if(node.pointer[0]==)
+
+		public boolean isRoot(){ //checks if the selected node is the root
+			if(node.pointer[0]==-1){ //**not sure about this
+				return true;
+			}
+			else{
+				return false;
+			} 
 		}
 	}
 }
