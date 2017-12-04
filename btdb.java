0cd603ee2df774ce2be46f1bb7c6e96f1c5a3213
magnus-untal <33887026+magnus-untal@users.noreply.github.com>
2017-12-04 19:42:07 +0800

    UPDATED BTDB FINAL VERSION

--- btdb.java
+++ btdb.java
@@ -23,20 +23,35 @@ public class btdb{
 					
 					long key = in.nextLong();
 					String toStore = in.nextLine();
-
-					long recordNum = valueFile.getRecord();
-					valueFile.writeToFile(toStore.trim().getBytes("UTF8"));
-					bTreeFile.insert(key,0,recordNum);
-
+					if(bTreeFile.findKey(key,0)==-1){
+						long recordNum = valueFile.getRecord();
+						bTreeFile.insert(key,0,recordNum);
+						valueFile.writeToFile(toStore.trim().getBytes("UTF8"));
+						System.out.printf("%d inserted.\n",key);
+					}
+					else
+						System.out.println("ERROR: key already exists.");
+					
 				}
 				else if(input.equals("select")){
 					long key = in.nextLong();
-					System.out.println(valueFile.readValues(key));
+					long location = bTreeFile.findKey(key,0);
+					if(location!=-1){
+						System.out.printf("%d %s\n",key,valueFile.readValues(location));
+					}
+					else
+						System.out.println("ERROR: key does not exist.");
 				}
 				else if(input.equals("update")){
 					long key = in.nextLong();
 					String toStore = in.nextLine();
-					valueFile.updateFile(toStore.trim().getBytes("UTF8"), key);
+					long location = bTreeFile.findKey(key,0);
+					if(location!=-1){
+						valueFile.updateFile(toStore.trim().getBytes("UTF8"), location);
+						System.out.printf("%d updated.\n", key);
+					}
+					else
+						System.out.println("ERROR: key does not exist.");
 				}
 				else{
 					System.out.println("ERROR: invalid command");
