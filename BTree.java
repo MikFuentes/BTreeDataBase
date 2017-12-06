c3e4d872b4f2208902f85f2b382d1204c3d724f0
magnus-untal <33887026+magnus-untal@users.noreply.github.com>
2017-12-06 13:06:53 +0800

    partial-test

--- BTree.java
+++ BTree.java
@@ -2,10 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class BTree{
+	private final int ORDER = 7;
+	private final int CENTER = (ORDER-1)/2;
 	private final long RECORD_COUNT_OFFSET = 0;
 	private final long INITIAL_OFFSET = 16;
-	private final int ORDER = 7;
-	private final long NODE_LENGTH = (3*ORDER-1)*8;
+	private final long NODE_LENGTH = (3*ORDER-1)*8;	
+
 	private long nodeCount, rootFinder;
 	private RandomAccessFile bTreeFile;
 	private ArrayList<Node> nodeList;
@@ -30,8 +32,8 @@ public class BTree{
 				rootFinder = 0;
 				bTreeFile.writeLong(nodeCount);
 				bTreeFile.writeLong(rootFinder);
-				Node initial = new Node();
-				writeNode(initial, 0);
+				Node initial = new Node(0);
+				writeNode(initial);
 				initial = null;
 				nodeList = new ArrayList<>();
 				childInformation = new ArrayList<>();
@@ -47,66 +49,88 @@ public class BTree{
 	*uses other methods to check for existing keys and write to files
 	*if a key value already in the node is inserted, prints output indicating that the key value already exists
 	*
-	*@param key		long indicating a key value of a node
+	*@param key				long indicating a key value of a node
 	*@param nodeLocation	long value indicating a node's location in the B-tree		
-	*@param offset		long value indicating a node's location in the values file
+	*@param offset			long value indicating a node's location in the values file
 	*@throws IOException
+	*@see IOException
 	*/
-	public void insert(long key, long nodeLocation, long offset) throws IOException{// method for inserting keys and their respective offset **not complete
+	public void insert(long key, long nodeLocation, long offset) throws IOException{// method for inserting keys and their respective offset **complete for now
 		
 		Node checker = readNode(nodeLocation);
-		nodeList.add(checker);
-		childInformation.add(nodeLocation);
+
 		if(!checker.hasChild()){
-			for(int i = ORDER-2; i>=0; i--){
-				if(checker.keys[i] == key){
-					System.out.println("ERROR: "+ key +" already exists");
-				}
-				if(checker.keys[i] == -1){
-					if(i==0){
-						checker.keys[i] = key;
-						checker.recordsOffset[i] = offset;
-					}
-					continue;
-				}
-				if(key<checker.keys[i]){
-					checker.keys[i+1] = checker.keys[i];
-					checker.recordsOffset[i+1] = checker.recordsOffset[i];
-					if(i==0){
-						checker.keys[i] = key;
-						checker.recordsOffset[i] = offset;
-					}
-				}
-				else{
-					checker.keys[i+1] = key;
-					checker.recordsOffset[i+1] = offset;
-					break;
-				}
-			}
+			checker.insertKey(key,offset);	
 		}
+
 		else{
 			long newLocation = checker.findChild(key);
 			checker = null;
 			insert(key,newLocation,offset);
 		}
+
 		if(checker!=null){
 			if(checker.keys[ORDER-1]!=-1)
 				split(checker);
 			else{
-				writeNode(checker,nodeLocation);
+				writeNode(checker);
+			}
+			checker = null;
+		}
+		
+	}
+
+	private void split(Node node) throws IOException{ //method for spliiting B-Tree **to be continued
+		if(node.parentPointer == -1){
+			Node rightChild = new Node(nodeCount);
+			nodeCount++;
+			Node root = new Node(nodeCount);
+			nodeCount++;
+			node.transferInformation(root,rightChild);
+			node.parentPointer = root.nodeID;
+			rightChild.parentPointer = root.nodeID;
+			root.addChild(node.nodeID);
+			root.addChild(rightChild.nodeID);
+			writeNode(node);
+			writeNode(rightChild);
+			writeNode(root);
+			writeHeader(root);
+			node = null;
+			rightChild = null;
+			root = null;
+		}
+		else{
+			Node parent = readNode(node.parentPointer);
+			Node rightChild = new Node(nodeCount);
+			nodeCount++;
+			node.transferInformation(parent,rightChild);
+			if(parent.keys[ORDER-1]!=-1){
+				split(parent);
 			}
 		}
-		nodeList.clear();
-		childInformation.clear();
 	}
-	public void split(Node node){ //method for spliiting B-Tree **to be continued
 
+	private void writeHeader(Node root) throws IOException{
+		bTreeFile.seek(0);
+		bTreeFile.writeLong(nodeCount);
+		bTreeFile.writeLong(root.nodeID);
 	}
+	public long findRoot()throws IOException{
+		bTreeFile.seek(8);
+		return bTreeFile.readLong();
+	}
+	
 	public long findKey(long key, long location) throws IOException{
 		Node checker = readNode(location);
 		for(int i = 0; i<ORDER-1; i++){
 			if(checker.keys[i]==key)
 				return checker.recordsOffset[i];
+			else if((checker.keys[i]<key&&checker.keys[i+1]==-1)
+				&&checker.childID[i+1]!=-1){
+				long newLocation = checker.childID[i+1];
+				checker = null;
+				return findKey(key, newLocation);
+			}
 			else if(checker.keys[i]>key&&checker.childID[i]!=-1){
 				long newLocation = checker.childID[i];
 				checker = null;
@@ -132,8 +156,8 @@ public class BTree{
 	*@param node		specified node to output values from
 	*@param location	long that indicates the node's location in the B-Tree
 	*/
-	public void writeNode(Node node, long location) throws IOException{// used when writing to file
-		bTreeFile.seek(INITIAL_OFFSET+location*NODE_LENGTH);
+	private void writeNode(Node node) throws IOException{// used when writing to file
+		bTreeFile.seek(INITIAL_OFFSET+node.nodeID*NODE_LENGTH);
 		bTreeFile.writeLong(node.parentPointer);
 		for(int i = 0; i<ORDER; i++){
 			bTreeFile.writeLong(node.childID[i]);
@@ -143,6 +167,7 @@ public class BTree{
 			}
 		}
 	}
+
 	/**
 	*Method readNode returns values stored in a file and writes them to a new node
 	*the new node is placed in a specified location in the B-tree
@@ -157,9 +182,10 @@ public class BTree{
 	*@param location	long that indicates the node's location in the B-Tree
 	*@return toReturn 	returns node read from file
 	*/
-	public Node readNode(long location) throws IOException{//used when inserting to nodes
+
+	private Node readNode(long location) throws IOException{//used when inserting to nodes
 		bTreeFile.seek(INITIAL_OFFSET+location*NODE_LENGTH);
-		Node toReturn = new Node();
+		Node toReturn = new Node(location);
 		toReturn.parentPointer = bTreeFile.readLong();
 		for(int i = 0; i<ORDER; i++){
 			toReturn.childID[i] = bTreeFile.readLong();
@@ -184,31 +210,80 @@ public class BTree{
 	*/
 	class Node{ //node class
 		private long[] keys, childID, recordsOffset;
-		private long parentPointer;
-		public Node(){ //each node has 3 long[]
+		private long parentPointer, nodeID;
+		private Node(long nodeLocation){ //each node has 3 long[]
 			keys = new long[ORDER];
 			childID = new long[ORDER];
 			recordsOffset = new long[ORDER];
 			parentPointer = -1;
-			
+			nodeID = nodeLocation;
 			for(int i = 0; i<ORDER; i++){
 				keys[i] = -1;
 				childID[i] = -1;
 				recordsOffset[i] = -1;
 			}
 		}
-		public boolean hasChild(){
+
+		private boolean hasChild(){
 			if(childID[0]!=-1)
 				return true;
 			return false;
 		}
-		public long findChild(long key){
+
+		private long findChild(long key){
 			long id = -1;
 			for(int i = 0; i<ORDER-1; i++){
 				if(key>keys[i]&&key<keys[i+1])
 					return childID[i];
 			}
 			return id;
+		}
+
+		private void addChild(long nodeLocation){
+			for(int i = 0; i<ORDER; i++){
+				if(childID[i]==-1){
+					childID[i] = nodeLocation;
+					break;
+				}
+			}
+		}
+
+		private void insertKey(long key, long offset){
+			for(int i = ORDER-2; i>=0; i--){
+				if(keys[i] == -1){
+					if(i==0){
+						keys[i] = key;
+						recordsOffset[i] = offset;
+						
+					}
+					continue;
+				}
+				if(key<keys[i]){
+					keys[i+1] = keys[i];
+					recordsOffset[i+1] = recordsOffset[i];
+					if(i==0){
+						keys[i] = key;
+						recordsOffset[i] = offset;
+					}
+				}
+				else{
+					keys[i+1] = key;
+					recordsOffset[i+1] = offset;
+					break;
+				}
+			}
+		}
+		private void transferInformation(Node root, Node sibling){
+			root.insertKey(keys[CENTER],recordsOffset[CENTER]);
+			keys[CENTER] = -1;
+			recordsOffset[CENTER] = -1;
+			for(int i = CENTER+1; i<ORDER; i++){
+				sibling.insertKey(keys[i],recordsOffset[i]);
+				sibling.addChild(childID[i]);
+				keys[i] = -1;
+				recordsOffset[i] = -1;
+				childID[i] = -1;
+			}
 		}
 	}
 }
